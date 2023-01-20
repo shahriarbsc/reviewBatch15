e58a5f61bfe81357e39943e915e8708cd654d477
shahriarbsc <shahriar.bsc@gmail.com>
2023-01-19 20:39:45 -0500

    loops

---
+++ src/com/syntax/review4/ForLoop.java
@@ -0,0 +1,42 @@
+package com.syntax.review4;
+
+public class ForLoop {
+
+
+    public static void main(String[] args) {
+        // best loop- used when we know how many times we want tto repeat block of codes
+
+
+        for (int i = 2; i <= 20; i += 5) {
+            System.out.println(i);
+        }
+
+        //break and continue
+        for (int i = 2; i <=20 ; i+=5) {
+            System.out.println(i);
+            if (i==12){
+                System.out.println("i is equal to 12-I am breaking my loop");
+                break;
+            }
+        }
+
+
+        //continue: skips current iteration.
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println(i);
+                continue; // java goes back to the begining of the loop
+                // and skips the rest of the code that is inside loop body
+
+            }
+            System.out.println("Hello");
+            System.out.println("How are you");
+            System.out.println("Hope you well");
+
+        }
+    }
+
+}
---
+++ src/com/syntax/review4/NestedLoop.java
@@ -0,0 +1,35 @@
+package com.syntax.review4;
+
+public class NestedLoop {
+
+    public static void main(String[] args) {
+        // nested loop- loop inside another loop
+
+        // inner loop ALWAYS depends on outer loop
+
+        for (int i = 1; i <=3 ; i++) { //outer loop
+            System.out.println(i);
+
+            for (int j = 1; j <=4 ; j++) { // inner loop
+                System.out.println(j);
+            }
+        }
+
+        System.out.println("---------------------------");
+// outer loop controls number of complete iteration 
+
+        for (int i = 1; i <5 ; i++) {
+            System.out.println("Hello");
+
+            for (int j = 1; j <=2 ; j++) {
+                System.out.println("Bye");
+            }
+
+
+
+        }
+
+
+
+    }
+}
